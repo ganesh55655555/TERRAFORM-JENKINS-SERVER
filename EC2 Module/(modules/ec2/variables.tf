6822b0e3ee753a52af5a82010afac8fc4cef65b5
ganesh55655555 <151905631+ganesh55655555@users.noreply.github.com>
2025-01-28 23:21:37 +0530

    Create variables.tf

---
+++ EC2 Module/(modules/ec2/variables.tf
@@ -0,0 +1,10 @@
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
