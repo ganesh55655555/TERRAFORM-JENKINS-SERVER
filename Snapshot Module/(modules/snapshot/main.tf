7e09d77613418d39f4ca8f8ad10b15a1309b7efb
ganesh55655555 <151905631+ganesh55655555@users.noreply.github.com>
2025-01-28 23:17:43 +0530

    Update main.tf

--- Snapshot Module/(modules/snapshot/main.tf
+++ Snapshot Module/(modules/snapshot/main.tf
@@ -1,36 +1,43 @@
+
+data "aws_caller_identity" "current" {}
+resource "aws_backup_vault" "main" {
+  name = "${var.snapshot_name}-vault"
+}
+
 resource "aws_backup_plan" "daily" {
   name = var.snapshot_name
 
   rule {
     rule_name         = "daily-backup"
     target_vault_name = aws_backup_vault.main.name
-    schedule          = "cron(0 12 * * ? *)"
+    schedule          = "cron(0 12 * * ? *)"  # Daily backup at 12:00 UTC
   }
 }
 
-resource "aws_backup_vault" "main" {
-  name = "${var.snapshot_name}-vault"
-}
-
 resource "aws_backup_selection" "ebs" {
-  name          = "daily-ebs-backup"
+  name     = "daily-ebs-backup"
   iam_role_arn  = aws_iam_role.backup_role.arn
-  backup_plan_id = aws_backup_plan.daily.id
+  plan_id  = aws_backup_plan.daily.id  # Use correct argument name
 
-  resources = [var.volume_id]
+  resources = [
+ "arn:aws:ec2:${var.region}:${data.aws_caller_identity.current.account_id}:volume/${var.volume_id}"
+  ]
 }
+
 
 resource "aws_iam_role" "backup_role" {
   name = "backup-role"
 
   assume_role_policy = jsonencode({
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "backup.amazonaws.com"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "backup.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
       }
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
 }
 
@@ -38,3 +45,4 @@ resource "aws_iam_role_policy_attachment" "backup_attach" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
   role       = aws_iam_role.backup_role.name
 }
+
