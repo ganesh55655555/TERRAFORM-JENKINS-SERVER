fd376b408090aa6a927f48cb3ae21beff2fd4a31
ganesh55655555 <151905631+ganesh55655555@users.noreply.github.com>
2025-01-28 23:22:50 +0530

    Update outputs.tf

--- Root module/outputs.tf
+++ Root module/outputs.tf
@@ -1,4 +1,8 @@
 output "jenkins_url" {
-  description = "Jenkins server public URL"
-  value       = "http://${module.ec2.elastic_ip}"
+  value = "http://${module.ec2.elastic_ip}"
+  description = "Public URL for the Jenkins server"
+}
+output "jenkins_public_ip" {
+  description = "Public Elastic IP of the Jenkins server"
+  value       = module.ec2.elastic_ip
 }
