4a7d5cddde435534dc474e7696427eef9a8de24e
ganesh55655555 <151905631+ganesh55655555@users.noreply.github.com>
2025-01-28 23:22:24 +0530

    Update Main.tf

--- Root module/Main.tf
+++ Root module/Main.tf
@@ -1,23 +1,17 @@
 provider "aws" {
-  region = "us-east-1" # Update with your preferred region
+  region = "eu-north-1" # Update with your preferred region
 }
 
-module "network" {
-  source = "./modules/network"
-}
 
 module "ec2" {
   source          = "./modules/ec2"
-  vpc_id          = module.network.vpc_id
-  public_subnet   = module.network.public_subnet_id
   key_name        = var.key_name
   instance_type   = var.instance_type
-  elastic_ip      = var.elastic_ip
-  github_repo_url = var.github_repo_url
 }
 
 module "snapshot" {
   source        = "./modules/snapshot"
-  volume_id     = module.ec2.volume_id
+  volume_id     = "vol-05933014c0823c62e" # Replace with your volume ID
   snapshot_name = var.snapshot_name
 }
+
