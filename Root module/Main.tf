e935e101a7c4c9639e2940da7153cbba8d84c9b3
ganesh55655555 <151905631+ganesh55655555@users.noreply.github.com>
2025-01-31 11:53:16 +0530

    Update Main.tf

--- Root module/Main.tf
+++ Root module/Main.tf
@@ -1,17 +1,15 @@
 provider "aws" {
-  region = "eu-north-1" # Update with your preferred region
+  region = "eu-north-1"  # Update with your preferred region
 }
 
-
 module "ec2" {
-  source          = "./modules/ec2"
-  key_name        = var.key_name
-  instance_type   = var.instance_type
+  source        = "./modules/ec2"
+  key_name      = var.key_name
+  instance_type = var.instance_type
 }
 
 module "snapshot" {
   source        = "./modules/snapshot"
-  volume_id     = "vol-05933014c0823c62e" # Replace with your volume ID
+  volume_id     = aws_instance.jenkins.ebs_block_device[0].volume_id  
   snapshot_name = var.snapshot_name
 }
-
